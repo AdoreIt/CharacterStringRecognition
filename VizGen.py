ab73dde28b6b7d1fa30a6c3a64c40c663d54caf4
AdoreIt <NatalieLytvyniuk@gmail.com>
2019-06-20 00:21:43 +0300

    Fix image and letters generation

--- VizGen.py
+++ VizGen.py
@@ -10,39 +10,71 @@ def generate_string(alphabet, length):
 def draw_letters(letters_string):
     font = cv2.FONT_HERSHEY_COMPLEX
     font_scale = 1
-    margin = 4
+    margin = 1
     thickness = 2
     color = (0, 0, 0)
 
-    size = cv2.getTextSize(letters_string, font, font_scale, thickness)
+    letter_imgs = {}
 
-    text_width = size[0][0]
-    text_height = size[0][1]
+    for letter in letters_string:
+        if letter not in letter_imgs:
+            print(letter)
+            size = cv2.getTextSize(letter, font, font_scale, thickness)
 
-    image = np.zeros((text_height + margin, text_width + margin, 3), np.uint8)
-    image.fill(255)
+            text_width = size[0][0]
+            text_height = size[0][1]
 
-    textX = int((image.shape[1] - text_width - margin / 2) / 2)
-    textY = int((image.shape[0] + text_height - margin / 2) / 2)
+            char_img = np.zeros((text_height + margin, text_width + margin, 3),
+                                np.uint8)
+            char_img.fill(255)
 
-    cv2.putText(image, letters_string, (textX, textY), font, font_scale, color,
-                thickness)
+            print("shape {0}:{1}".format(char_img.shape[1], char_img.shape[0]))
 
-    return image
+            textY = text_height
+            textX = 0
+
+            cv2.putText(char_img, letter, (textX, textY), font, font_scale,
+                        color, thickness)
+
+            show_image(char_img)
+            letter_imgs[letter] = char_img
+
+    return letter_imgs
+
+
+def concatenate_images(letters_string, images_dict):
+    images = [images_dict[letter] for letter in letters_string]
+
+    height = max(image.shape[0] for image in images)
+    width = sum(image.shape[1] for image in images)
+
+    concatenated_image = np.zeros((height, width, 3), np.uint8)
+
+    w_offset = 0
+    for image in images:
+        h, w, c = image.shape
+        concatenated_image[0:h, w_offset:w_offset + w] = image
+        w_offset += w
+
+    return concatenated_image
 
 
 def generate_image(alphabet, length):
-    return draw_letters(generate_string(alphabet, 5))
+    generated_string = generate_string(alphabet, 5)
+    print(generated_string)
+    letters_dict = draw_letters(generated_string)
+    image = concatenate_images(generated_string, letters_dict)
+    return image
 
 
 def noise_image(image, mean=0, sigma=0):
-    im_width, im_height, channels = get_im_size(image)
+    im_height, im_width, channels = image.shape
 
     gaussian = np.random.normal(mean, sigma, (im_height, im_width))
 
     noisy_image = np.zeros(image.shape, np.float32)
 
-    if len(channels) == 2:
+    if channels == 2:
         noisy_image = image + gaussian
     else:
         noisy_image[:, :, 0] = image[:, :, 0] + gaussian
@@ -62,7 +94,3 @@ def show_image(image, window_name=""):
     plt.axis("off")
     plt.show()
     plt.close()
-
-
-def get_im_size(image):
-    return (image.shape[1], image.shape[0], image.shape)
